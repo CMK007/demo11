0c497e8c5e1fcc6b4469f94d59113d91bf379e18
CMK <148140994+Arzen1@users.noreply.github.com>
2024-07-07 04:08:41 +0300

    yeni method

--- YyManager.java
+++ YyManager.java
@@ -5,5 +5,9 @@ public class YyManager extends Manager{
     public void xyz(){
         System.out.println("yyyyyy");
     }
+
+    public void xyz2(){
+        System.out.println("yyyyyy");
+    }
     
 }
